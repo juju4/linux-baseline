9950cb7b56d085b330534ada9cb95915c147a5fe
juju4 <juju4@users.noreply.github.com>
2018-09-30 13:44:08 -0400

    profile: add file attribute, fix typo

--- controls/profile_spec.rb
+++ controls/profile_spec.rb
@@ -1,5 +1,6 @@
 
 profile_check = attribute('profile_check', default: true, description: 'Control /etc/profile settings')
+profile_file = attribute('profile_file', default: '/etc/profile', description: 'Control which file to check for settings')
 profile_tmout = attribute('profile_tmout', default: 3600, description: 'Value for shell Timeout')
 profile_histfilesize = attribute('profile_histfilesize', default: 5000, description: 'Value for HISTFILESIZE')
 profile_histsize = attribute('profile_histsize', default: 5000, description: 'Value for HISTSIZE')
@@ -7,15 +8,15 @@ profile_histsize = attribute('profile_histsize', default: 5000, description: 'Va
 control 'profile-01' do
   impact 1.0
   title 'Shell profile settings'
-  describe file('/etc/profile') do
+  describe file(profile_file) do
     it { should exist }
     its(:content) { should match "readonly TMOUT=#{profile_tmout}" }
     its(:content) { should match(/export HISTCONTROL=$/) }
-    its(:content) { should match 'export HISTFILE=$HOME/.bash_history' }
+    its(:content) { should match 'export HISTFILE=\$HOME/.bash_history' }
     its(:content) { should match "export HISTFILESIZE=#{profile_histfilesize}" }
     its(:content) { should match(/export HISTIGNORE=$/) }
     its(:content) { should match "export HISTSIZE=#{profile_histsize}" }
-    its(:content) { should match 'export HISTTIMEFORMAT="%a %b %Y %T %z "' }
+    its(:content) { should match 'export HISTTIMEFORMAT="%a %b %Y %T %z"' }
   end
   only_if { profile_check == true }
 end
